7814706b4376b9c278ea8e6935ab67f630f4f872
Hyojin <88768022+su-no@users.noreply.github.com>
2022-12-07 20:02:24 +0900

    Fix: TodoList 반복되는 로직 병합

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,37 +1,21 @@
 import React from 'react';
 import Todo from './Todo';
 
-export default function TodoList({ list, todos, setTodos }) {
-  const ACTIVE = 'Active 🔥';
-
+export default function TodoList({ listName, todos, setTodos }) {
   return (
     <div className='todo-list'>
-      <h2 className='todo-list__title'>{list}</h2>
-      {list === ACTIVE
-        ? // Active 리스트는 isDone이 false인 리스트
-          todos
-            .filter((t) => t.isDone === false)
-            .map((t) => (
-              <Todo
-                todo={t.todo}
-                isDone={t.isDone}
-                setTodos={setTodos}
-                key={t.id}
-                id={t.id}
-              />
-            ))
-        : // Done 리스트는 isDone이 false인 리스트
-          todos
-            .filter((t) => t.isDone === true)
-            .map((t) => (
-              <Todo
-                todo={t.todo}
-                isDone={t.isDone}
-                setTodos={setTodos}
-                key={t.id}
-                id={t.id}
-              />
-            ))}
+      <h2 className='todo-list__title'>{listName}</h2>
+      {todos
+        .filter((t) => t.isDone === (listName === 'active' ? false : true))
+        .map((t) => (
+          <Todo
+            todo={t.todo}
+            isDone={t.isDone}
+            setTodos={setTodos}
+            key={t.id}
+            id={t.id}
+          />
+        ))}
     </div>
   );
 }
