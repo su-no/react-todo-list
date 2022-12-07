7f10bb169a6ecbf7faa49ad790898efc0bf7f1a7
Hyojin <88768022+su-no@users.noreply.github.com>
2022-12-07 16:23:45 +0900

    Style: 투두리스트 초기값 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import { useState } from 'react';
+import { v4 as uuid } from 'uuid';
 import './App.css';
 import AddForm from './components/AddForm';
 import TodoList from './components/TodoList';
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([
+    {
+      id: uuid(),
+      isDone: false,
+      todo: '리액트 강의 듣기',
+    },
+    {
+      id: uuid(),
+      isDone: true,
+      todo: '투두 리스트 만들기',
+    },
+    {
+      id: uuid(),
+      isDone: true,
+      todo: '마라샹궈 먹기',
+    },
+  ]);
   const [ACTIVE, DONE] = ['Active 🔥', 'Done ✅'];
 
   return (
