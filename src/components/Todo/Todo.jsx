43e02db809878ebde3e66cd5b4d4cccaaf1c8bb5
Hyojin <88768022+su-no@users.noreply.github.com>
2022-12-08 15:53:27 +0900

    Correct: cancelTodo 함수 오타 수정

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -4,13 +4,13 @@ import './style.css';
 
 export default function Todo({ todo, isDone, id, setTodos }) {
   // todo 제거하는 함수
-  const deleteTodo = (e) => {
+  const deleteTodo = () => {
     // 해당 todo를 제외한 todos 배열을 업데이트
     setTodos((prev) => prev.filter((t) => t.id !== id));
   };
 
   // 완료된 todo 취소하는 함수
-  const cancleTodo = (e) => {
+  const cancelTodo = () => {
     setTodos((prev) =>
       prev.map((t) => {
         // 해당 todo의 isDone을 false로 변경
@@ -24,7 +24,7 @@ export default function Todo({ todo, isDone, id, setTodos }) {
   };
 
   // todo를 완료하는 함수
-  const doneTodo = (e) => {
+  const doneTodo = () => {
     setTodos((prev) =>
       prev.map((t) => {
         // 해당 todo의 isDone을 true로 변경
@@ -44,7 +44,7 @@ export default function Todo({ todo, isDone, id, setTodos }) {
         <Button handleClick={deleteTodo} value='삭제' />
         {isDone ? (
           // 완료된 todo는 취소버튼 표시
-          <Button handleClick={cancleTodo} value='취소' />
+          <Button handleClick={cancelTodo} value='취소' />
         ) : (
           // 완료되지 않은 todo는 완료버튼 표시
           <Button handleClick={doneTodo} value='완료' />
