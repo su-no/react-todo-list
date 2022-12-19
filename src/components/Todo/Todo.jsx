e8618310cc84bb145747c0e50f95ec05308171ff
Hyojin <88768022+su-no@users.noreply.github.com>
2022-12-19 23:07:23 +0900

    Refactor: cancelTodo, doneTodo -> updateTodo로 통합

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -9,31 +9,17 @@ export default function Todo({ todo, isDone, id, setTodos }) {
     setTodos((prev) => prev.filter((t) => t.id !== id));
   };
 
-  // 완료된 todo 취소하는 함수
-  const cancelTodo = () => {
+  // todo 상태를 업데이트 하는 함수
+  const updateTodo = () => {
     setTodos((prev) =>
       prev.map((t) => {
         // 해당 todo의 isDone을 false로 변경
         if (t.id === id) {
-          return { ...t, isDone: false };
+          return { ...t, isDone: !t.isDone };
         }
         // 다른 todo는 그대로 반환
         return t;
-      }),
-    );
-  };
-
-  // todo를 완료하는 함수
-  const doneTodo = () => {
-    setTodos((prev) =>
-      prev.map((t) => {
-        // 해당 todo의 isDone을 true로 변경
-        if (t.id === id) {
-          return { ...t, isDone: true };
-        }
-        // 다른 todo는 그대로 반환
-        return t;
-      }),
+      })
     );
   };
 
@@ -44,10 +30,10 @@ export default function Todo({ todo, isDone, id, setTodos }) {
         <Button handleClick={deleteTodo} value='삭제' />
         {isDone ? (
           // 완료된 todo는 취소버튼 표시
-          <Button handleClick={cancelTodo} value='취소' />
+          <Button handleClick={updateTodo} value='취소' />
         ) : (
           // 완료되지 않은 todo는 완료버튼 표시
-          <Button handleClick={doneTodo} value='완료' />
+          <Button handleClick={updateTodo} value='완료' />
         )}
       </div>
     </li>
